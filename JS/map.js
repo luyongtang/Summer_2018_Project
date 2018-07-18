c21ea424f4e928924cb6c1bb5c3ad0cec1dc3b5b
Yongtang Lu <wilsonluyongtang_2005@hotmail.com>
2018-07-18 16:01:25 -0400

    Defined a new object called GoogleMap (#2)
    1. Defined a new object called GoogleMap, with two constructor functions and one setter;
    2. The load() function is not completed yet.

--- JS/map.js
+++ JS/map.js
@@ -5,3 +5,26 @@ function myMap() {
     };
     var map = new google.maps.Map($("#googleMap")[0], mapProp);
 }
+
+// To defined a new object called GoogleMap
+function GoogleMap () {
+    // Attribute
+    this.id = "";
+
+    // the constructor without parameters
+    this.initWithoutID = function () {
+        this.id = "googleMap"; // default id
+    };
+    // the constructor with one parameter
+    this.initWithID = function (newID) {
+        this.id = newID;
+    };
+    // setter
+    this.setMap = function (newID) { 
+        this.id = newID;
+    };
+    // .load() function
+    this.load = function () {
+        // I have no idea of this function for now. Please advise.
+    };
+}
