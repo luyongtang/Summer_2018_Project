e394e1e00d20cfb702e534bc28f7b5cdc7c2380d
Yongtang Lu <wilsonluyongtang_2005@hotmail.com>
2018-07-24 19:47:35 +0000

    Use http.post method to fetch data from the server
    The connection is successful; however, it seems there is no stories information in the data return.

--- JS/driver.js
+++ JS/driver.js
@@ -3,18 +3,19 @@ var app = angular.module('myApp', ['ngRoute']);
 //use data_result as the result received from the server
 //console.log(data_result);
 app.controller("map_box",function($scope,$http){
-	/* To be used later on once the file is uploaded to server.
-	$http.post("JS/extract.json", JSON.stringify("{}")).then(
-		function successCallback(response) {
-			//console.log(response);
-			console.log(response);
-		},
-		function errorCallBack(response) {
-			console.log(response);
-		}
-	)
-	*/
-})
+    /* To be used later on once the file is uploaded to server.*/
+    $http.post("http://eaglepulse.com/a_construct/units/json_result_set/json_result_set.php", JSON.stringify("{}")).then(
+        function successCallback(response) {
+            //console.log(response);
+            console.log(response);
+        },
+        function errorCallBack(response) {
+            console.log("The connection is not successful!");
+        }
+    );
+    
+    
+});
 
 
 /*
